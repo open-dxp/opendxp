56bfa903a78c8e8601c15412ed53d0f14102127e
Stefan Hagspiel <shagspiel@dachcom.ch>
2025-12-11 22:26:57 +0100

    cleanup

--- .github/ci/scripts/setup-opendxp-environment.sh
+++ .github/ci/scripts/setup-opendxp-environment.sh
@@ -3,11 +3,12 @@
 set -eu
 
 mkdir -p var/config
+mkdir -p config/local/
 
 cp -r .github/ci/files/config/. config
-mkdir -p config/local/
 cp -r .github/ci/files/templates/. templates
 cp -r .github/ci/files/bin/console bin/console
 cp -r .github/ci/files/src/. src
 cp -r .github/ci/files/public/. public
+
 cp .github/ci/files/.env ./
